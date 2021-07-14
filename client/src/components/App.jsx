a5f9da94c750c0c4862a23137e594c7bec1358e7
Gareth Rogers <garethdavisrogers@gmail.com>
2021-07-13 20:28:54 -0500

    working out transitions

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,14 +1,62 @@
 import React from "react";
+import mean_mug from "../../public/mean_mug_cropped.jpg";
+import downArrow from "../../public/thick-arrow-pointing-down.png";
+import Typical from "react-typical";
+import Skills from "./Skills";
 
 class App extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      blueArrow: false,
+    };
+  }
+
+  blueArrow = () => {
+    this.setState({ blueArrow: true });
+  };
+  textTimer = () => {
+    setInterval(this.blueArrow, 13000);
+  };
+
+  componentDidMount() {
+    this.textTimer();
   }
 
   render() {
-    return <div>Hello World</div>;
+    const { blueArrow } = this.state;
+    return (
+      <div className="page">
+        <div className="header-image">
+          <button
+            className="skip-intro"
+            onClick={() => this.setState({ blueArrow: true })}
+          >
+            Skip Intro
+          </button>
+          <img src={mean_mug} className="mean-mug" />
+        </div>
+        <div className="typical-intro">
+          {!blueArrow && (
+            <Typical
+              steps={[
+                1000,
+                "Hi...",
+                1000,
+                "I'm Gareth",
+                1000,
+                "And I'm a software engineer",
+                2000,
+                "Follow me, please",
+              ]}
+            />
+          )}
+        </div>
+        {blueArrow && <img className="pointing-arrow" src={downArrow} />}
+        <Skills />
+      </div>
+    );
   }
 }
 
---
+++ client/src/components/Skills.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Skills = (props) => {
+  return (
+    <div>
+      <header>Stats and Skill Ratings</header>
+      <div>
+        <label>React.js: </label>
+        <label>Node.js/Express: </label>
+        <label>Postgresql</label>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
